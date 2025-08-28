1eaa2678e5978cd730cba0855e0ab95cff073182
Tomáš Weber <tomas.weber@email.cz>
2025-08-28 15:36:36 +0200

    Initial commit

---
+++ src/test/java/com/engeto/genesis_resources/GenesisResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeto.genesis_resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesisResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
